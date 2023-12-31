3a8ae7746b66cb10fd4a1ae7af573f58e0252658
Azadshekhar <shekharmaurya8978@gmail.com>
2023-12-31 11:46:57 +0530

    number variables in dart

--- DartFundamentals.dart
+++ DartFundamentals.dart
@@ -1,6 +1,20 @@
 // Entry point of Program execution 'void main()'
 void main(){
-  print('Namaste India');
+  print('Namaste India'); // for printing anything like java,c,c++ .....
   print("Namaste India");
+
+  int a=21;
+  num b=21;
+  var c=21;
+  print(a);
+  print(b);
+  print(c);
+
+  double aa=10;
+  num bb=10.0;
+  var cc=10.0;
+  print(aa);
+  print(bb);
+  print(cc);
 
 }
